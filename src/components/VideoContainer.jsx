f061136de45fa91e13b93035188b1b24721d01a3
Shivankit Agarwal <shivankit1210@gmail.com>
2024-09-03 16:09:18 +0530

    Apply redux for Feed Video

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -3,18 +3,35 @@ import axios from "axios";
 import { YOUTUBE_VIDEO_API } from "../constants/youtube";
 import VideoCard from "./VideoCard";
 import { Link } from "react-router-dom";
+import { API_KEY } from "../constants/youtube";
+import { useDispatch } from "react-redux";
+import { setHomeVideo } from "../utils/appSlice";
 
 const VideoContainer = () => {
   const [video, setVideo] = useState([]);
+
+  const dispatch= useDispatch();
 
   const fetchVideo = async () => {
     try {
       const res = await axios.get(`${YOUTUBE_VIDEO_API}`);
       setVideo(res?.data?.items);
+      dispatch(setHomeVideo(res?.data?.items))
     } catch (error) {
       console.log(error);
     }
   };
+
+  const fetchVideoByCategory = async ()=> {
+
+    try {
+      const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=YOURKEYWORD&type=video&key=${API_KEY}`)
+
+    } catch (error) {
+      
+    }
+
+  }
 
   useEffect(() => {
     fetchVideo();
--- src/utils/appSlice.jsx
+++ src/utils/appSlice.jsx
@@ -4,13 +4,22 @@ const appSlice= createSlice({
     name:"app",
     initialState:{
         open:true,
+        video:[],
+        category:"All",
+
     },
     reducers:{
         togglesidebar:(state)=>{
             state.open= !state.open;
-        }
+        },
+        setHomeVideo:(state,action)=>{
+            state.video= action.payload;
+        },
+        setCategory:(state,action)=>{
+            state.category=action.payload;
+        },
     }
 })
 
-export const {togglesidebar} =appSlice.actions;
+export const {togglesidebar,setHomeVideo,setCategory  } =appSlice.actions;
 export default appSlice.reducer
